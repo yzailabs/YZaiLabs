24a2a9130bf72103f572dbb67dc5a2de41a3294d
fdl34543 <fdl34543@gmail.com>
2026-03-13 03:39:39 +0700

    YZaiLabs

--- api/app/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-JWT_SECRET = os.getenv("JWT_SECRET")
-JWT_ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
